7c27079b1978157f3210c44f31acab96f8feb484
Alex Curran <alexcurran.va@gmail.com>
2025-05-14 17:25:35 -0500

    Add dev logging with Morgan package

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "development") {
+    const morgan = require("morgan");
+
+    app.use(morgan("dev"));
+}
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode.`);
 });
