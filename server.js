95e24cf71b2e65f2b073890869f2bc80545f9c94
Alex Curran <alexcurran.va@gmail.com>
2025-05-14 17:01:08 -0500

    Add start and dev scripts for server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode.`);
+});
