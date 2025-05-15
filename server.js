eccb82489882e3a33c3384e16d9ca5d166ed5992
Alex Curran <alexcurran.va@gmail.com>
2025-05-15 10:06:58 -0500

    Set up ENABLE_REQUEST_LOGGING .env variable instead of relying on NODE_ENV

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.ENABLE_REQUEST_LOGGING === "true") {
     const morgan = require("morgan");
 
     app.use(morgan("dev"));
